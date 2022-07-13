92779861854ea205cce6f18478ec647c881acc01
Farhan Farasdak <farhan.farasdak@flash-coffee.com>
2022-07-13 07:17:33 +0700

    add click event listener

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const table = document.getElementById("display-table");
 const searchFilter = document.getElementById("search-filter");
+const pokemonImg = document.querySelector(".pokemon-img");
 const URL = "https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0"
 let searchWord = ""
+let pokemonData = []
 
 const fetchData = async () => {
   const getData = await fetch(URL);
@@ -20,25 +22,55 @@ const resetTable = () => {
   table.appendChild(tableHead)
 }
 
-const generateTable = async (word) => {
-  resetTable()
+const filterData = async (word) => {
   let resp = await fetchData()
-  resp.results.forEach( e => {
-    if(e.name.includes(word)){
-      const pokemon = document.createElement("tr")
-      const pokemonName = document.createElement("td")
-      const pokemonUrl = document.createElement("td")
-      pokemonName.innerText = e.name
-      pokemonUrl.innerText = e.url
-      pokemon.appendChild(pokemonName)
-      pokemon.appendChild(pokemonUrl)
-      table.appendChild(pokemon)
+  resp.results.forEach(e => {
+    if(e.name.includes(word.toLowerCase())){
+      let tempData = {
+        name: e.name,
+        url: e.url
+      }
+      pokemonData.push(tempData)
     }
   })
 }
 
-generateTable(searchWord)
+const generateTable = async () => {
+  resetTable();
+  await filterData(searchWord);
+  pokemonData.forEach( e => {
+    const pokemon = document.createElement("tr")
+    const pokemonName = document.createElement("td")
+    const pokemonUrl = document.createElement("td")
+    pokemonName.innerText = e.name
+    pokemonUrl.innerText = e.url
+    pokemonUrl.classList.add("my-url")
+    pokemonUrl.setAttribute("id", "pokemon")
+    pokemon.appendChild(pokemonName)
+    pokemon.appendChild(pokemonUrl)
+    table.appendChild(pokemon)
+  })
+}
+
+generateTable()
 searchFilter.addEventListener("keyup", (e) => {
   searchWord = e.target.value
-  generateTable(searchWord)
+  pokemonData = []
+  generateTable()
+})
+
+const fetchDetailPokemon = async (url) => {
+  const detailPokemon = await fetch(url)
+  return detailPokemon.json()
+}
+
+table.addEventListener("click", async (e) => {
+  if(e.target.id === "pokemon"){
+    let src = ""
+    await fetchDetailPokemon(e.target.innerText)
+      .then( resp => {
+        src = resp.sprites.back_default
+      })
+    pokemonImg.src = src
+  }
 })
